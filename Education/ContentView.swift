7657dfba0c94096fbf2a3a8a8c660f4ce9a5f74c
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-07-21 17:16:56 -0600

    Initial Commit

---
+++ Education/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Education
+//
+//  Created by Guillermo Ruiz Baires on 21/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Education/EducationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducationApp.swift
+//  Education
+//
+//  Created by Guillermo Ruiz Baires on 21/7/23.
+//
+
+import SwiftUI
+
+@main
+struct EducationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
