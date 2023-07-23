68ca8cdd6517daa9d9d676aae4cf9da50940fbcc
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-07-22 22:55:03 -0600

    Practicing
    Using Bindings, computed properties, List, Section, TexField, Stepper

--- Education/ContentView.swift
+++ Education/ContentView.swift
@@ -9,20 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var multiplicationTable = 2
-    @State private var numberOfRound = 5
-    @State private var roundAnswers = [Int]()
+//    @State private var numberOfRound = 5
+//    @State private var roundAnswers = [Int]()
     @State private var correctAnswer = 0
     @State private var userNumber = 0
+    @State private var text = ""
+    
+    var result: Int {
+        userNumber * multiplicationTable
+    }
     
     var body: some View {
         NavigationStack {
-            Stepper("Table", value: $multiplicationTable, step: 2)
-                .labelsHidden()
-            
-            TextField("Enter a number", value: $userNumber, format: .number)
-            
-            
-            
+            List {
+                Text("Welcome to multiply operations")
+                Section("Enter your number:") {
+                    TextField("", value: $userNumber, format: .number)
+                        .textFieldStyle(.roundedBorder)
+                }
+                Section("Multiply by:") {
+                    Stepper("\(multiplicationTable)", value: $multiplicationTable, in: 2...12)
+                }
+                Section("Result:") {
+                    Text("\(result)")
+                }
+            }
+            .navigationTitle("Education")
         }
     }
 }
