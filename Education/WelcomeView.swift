f0446f08f8d422fa869bdbe25cdf20664b6890c0
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-07-26 17:21:34 -0600

    Layout of Welcome View

---
+++ Education/WelcomeView.swift
@@ -0,0 +1,52 @@
+//
+//  WelcomeView.swift
+//  Education
+//
+//  Created by Guillermo Ruiz Baires on 26/7/23.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        ZStack {
+            
+            Image("swift_world", bundle: nil)
+                .resizable()
+                .aspectRatio(1 / 1, contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+                .saturation(0.5)
+                .blur(radius: 5)
+                .opacity(0.08)
+            
+            HStack {
+                Image(systemName: "table")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                    .background(Color(white: 0.9))
+                    .clipShape(Circle())
+                    .foregroundColor(.red)
+                
+                
+                VStack(alignment: .leading) {
+                    Text("Welcome to")
+                        .font(.headline)
+                        .bold()
+                    Text("Kuchi")
+                        .font(.largeTitle)
+                        .bold()
+                }
+                .foregroundColor(.red)
+                .lineLimit(1)
+                .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
