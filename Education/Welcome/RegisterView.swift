801b3b7f5349c4da2f4896885d6ec59c34adba81
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-07-27 19:13:08 -0600

    Styling the TextField

--- Education/Welcome/RegisterView.swift
+++ Education/Welcome/RegisterView.swift
@@ -15,8 +15,23 @@ struct RegisterView: View {
                 Spacer()
                 WelcomeMessageView()
                 TextField("Type your name", text: $name)
+                    .padding(
+                    EdgeInsets(
+                        top: 8, leading: 16, bottom: 8, trailing: 15))
+                    .background(Color.white)
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(.blue)
+                    )
+                    .shadow(
+                        color: Color.gray.opacity(0.4),
+                        radius: 3, x: 1, y: 2)
                 Spacer()
-            }.background(WelcomeBackgroundImage())
+            }
+            .padding()
+            .background(WelcomeBackgroundImage())
+            
     }
 }
 
