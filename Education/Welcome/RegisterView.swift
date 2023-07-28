f66816240b80345b9d8867d8360a6d2f1d1278f1
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-07-27 23:22:14 -0600

    Refactoring and creating custom modifier

--- Education/Welcome/RegisterView.swift
+++ Education/Welcome/RegisterView.swift
@@ -11,30 +11,25 @@ struct RegisterView: View {
     @State var name: String = ""
     
     var body: some View {
-            VStack {
-                Spacer()
-                WelcomeMessageView()
-                TextField("Type your name", text: $name)
-                    .padding(
-                    EdgeInsets(
-                        top: 8, leading: 16, bottom: 8, trailing: 15))
-                    .background(Color.white)
-                    .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(lineWidth: 2)
-                        .foregroundColor(.blue)
-                    )
-                    .shadow(
-                        color: Color.gray.opacity(0.4),
-                        radius: 3, x: 1, y: 2)
-                Spacer()
-            }
-            .padding()
-            .background(WelcomeBackgroundImage())
-            
+        VStack {
+            Spacer()
+            WelcomeMessageView()
+            TextField("Type your name....", text: $name)
+                .bordered()
+            Spacer()
+        }
+        .padding()
+        .background(WelcomeBackgroundImage())
+        
     }
 }
 
+struct KuchiTextStyle: TextFieldStyle {
+    public func _body(
+        configuration: TextField<Self._Label>) -> some View {
+            return configuration
+        }
+}
 struct RegisterView_Previews: PreviewProvider {
     static var previews: some View {
         RegisterView()
