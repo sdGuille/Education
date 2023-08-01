3afba4cb43082b1a80ef6ff3cc93aa55490ae739
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-08-01 09:31:37 -0600

    Practicing with the stepper and slider

---
+++ Education/PraticeView.swift
@@ -0,0 +1,37 @@
+//
+//  PraticeView.swift
+//  Education
+//
+//  Created by Guillermo Ruiz Baires on 1/8/23.
+//
+
+import SwiftUI
+
+struct PraticeView: View {
+    @State var amount: Double = 0
+    @State var quantity = 0.0
+    
+    var body: some View {
+        VStack{
+            HStack {
+                Text("0")
+                Slider(
+                    value: $amount,
+                    in: 0.0 ... 10.0,
+                    step: 0.5
+                )
+                
+                Text("10")
+            }
+            Text("\(amount.formatted())")
+//            Spacer()
+            Stepper("Quantity: \(quantity.formatted())", value: $quantity, in: 0 ... 10, step: 0.50)
+        }
+    }
+}
+
+struct PraticeView_Previews: PreviewProvider {
+    static var previews: some View {
+        PraticeView()
+    }
+}
